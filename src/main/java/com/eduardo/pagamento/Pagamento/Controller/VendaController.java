9bcf43e20b2fd39a42594b5932d554e8f6266c45
NoteEdu <eduardo.tsilva@hotmail.com>
2021-06-27 18:19:32 -0400

    Finalizando VendaController

--- src/main/java/com/eduardo/pagamento/Pagamento/Controller/VendaController.java
+++ src/main/java/com/eduardo/pagamento/Pagamento/Controller/VendaController.java
@@ -44,7 +44,7 @@ public class VendaController {
             ) {
         var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
 
-        Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "nome"));
+        Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "data"));
 
         Page<VendaVO> vendas = vendaService.findAll(pageable);
 
